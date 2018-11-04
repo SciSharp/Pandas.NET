89714cfc2e40dfa5ee4ca4149f34bfff74e9a6c7
Oceania2018 <haiping008@gmail.com>
2018-11-04 10:15:03 -0600

    init commit

---
+++ test/PandasNET.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PandasNET.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
