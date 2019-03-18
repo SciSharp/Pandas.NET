ad44cf30627bcdac3fed72bb39f7b1a94ca683ff
王杰 <wangjie916@163.com>
2019-03-18 15:26:37 +0800

    添加readme

--- src/Pandas.NET/Extensions/DataFrameMethods.cs
+++ src/Pandas.NET/Extensions/DataFrameMethods.cs
@@ -48,6 +48,14 @@ namespace PandasNet
         }
         #endregion
 
+        /// <summary>
+        /// 按列名对应值进行排序
+        /// </summary>
+        /// <param name="df"></param>
+        /// <param name="columns">列名数组</param>
+        /// <param name="ascending">升序/降序，默认升序</param>
+        /// <param name="kind">排序算法，默认快速排序</param>
+        /// <returns></returns>
         public static IDataFrame sort_values(this IDataFrame df, string[] columns, bool ascending = true, SortKind kind = SortKind.quicksort)
         {
             var sorter = new Sorter(df, columns.ToList(), ascending, kind);
@@ -55,7 +63,15 @@ namespace PandasNet
             return sort.ExcuteSort();
         }
 
-        public static IDataFrame sort_index(this IDataFrame df, int[] indexs, bool ascending = true, SortKind kind = SortKind.quicksort)
+        /// <summary>
+        /// 按列索引对应值进行排序 
+        /// </summary>
+        /// <param name="df"></param>
+        /// <param name="indexs">列名索引</param>
+        /// <param name="ascending">升序/降序，默认升序</param>
+        /// <param name="kind">排序算法，默认快速排序</param>
+        /// <returns></returns>
+        public static IDataFrame sort_values(this IDataFrame df, int[] indexs, bool ascending = true, SortKind kind = SortKind.quicksort)
         {
             var sorter = new Sorter(df, indexs.ToList(), ascending, kind);
             var sort = new DataFrameSort(df, sorter);
