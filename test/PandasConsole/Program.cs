77ae8b2ec231248fcecf7281256f3283d617e887
Brett Webster <brettwebst@gmail.com>
2024-04-03 21:56:04 -0500

    added drop test. Needed to set index in from_dict API method.

--- test/PandasConsole/Program.cs
+++ test/PandasConsole/Program.cs
@@ -37,9 +37,6 @@ namespace PandasConsole
                     }
                 })
                 .WithNotParsed(CommandRunners.HandleParseError);
-
-            var df = pd.DataFrame.from_dict("{'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}");
-            var df1 = df[new[] { "col_1" }];
         }
 
         public static class CommandRunners
