a81e5059bd61af3776541bdccde75d738de03da8
Brett Webster <brettwebst@gmail.com>
2024-07-27 06:24:03 -0500

    added pop test

---
+++ test/Pandas.NET.Test/DataFrames/DataFrame.test.pop.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using PandasNet;
+using Tensorflow;
+
+public class DataFrameTests
+{
+    [Fact]
+    public void TestPopMethod()
+    {
+        // Arrange
+        var dataFrameData = new List<Series>();
+        dataFrameData.Add(new Series(new float[] { 1, 2, 3, 4, 5 }, new Column("column1", typeof(float))));
+        dataFrameData.Add(new Series(new float[] { 6, 7, 8, 9, 10 }, new Column("column2", typeof(float))));
+        var dataFrame = new DataFrame(dataFrameData);
+
+        // Act
+        var poppedSeries = dataFrame.pop("column1");
+
+        // Assert
+        Assert.True(poppedSeries.array<float>().SequenceEqual<float>([1, 2, 3, 4, 5]));
+        Assert.False(dataFrame.columns.Where(c => c.Name == "column1").Any());
+        Assert.True(dataFrame.columns.Where(c => c.Name == "column2").Any());
+    }
+}
